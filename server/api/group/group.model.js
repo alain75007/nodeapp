05a55a0a22dc2a9231869f3331985c040c19e8de
Alain Beauvois <alain@itipic.com>
2015-04-08 14:24:39 +0200

    Add a ref to user schema in group schema

--- server/api/group/group.model.js
+++ server/api/group/group.model.js
@@ -3,7 +3,10 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var User = require('../user/user.model');
+
 var GroupSchema = new Schema({
+  _creator:  { type: Schema.Types.ObjectId, ref: 'User' },
   name: String,
   info: String,
   active: Boolean
