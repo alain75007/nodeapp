cadd11b4a1b3b121debe6277577bafc17e2884e5
Alain Beauvois <alain@itipic.com>
2015-04-08 14:25:01 +0200

    Add some seed for groups

--- server/config/seed.js
+++ server/config/seed.js
@@ -7,6 +7,7 @@
 
 var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
+var Group = require('../api/group/group.model');
 
 Thing.find({}).remove(function() {
   Thing.create({
@@ -33,17 +34,31 @@ Thing.find({}).remove(function() {
 User.find({}).remove(function() {
   User.create({
     provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  }, {
-    provider: 'local',
     role: 'admin',
     name: 'Admin',
     email: 'admin@admin.com',
     password: 'admin'
   }, function() {
-      console.log('finished populating users');
+      console.log('finished populating admin user');
     }
   );
-});
+  User.create({
+    provider: 'local',
+    name: 'Test User',
+    email: 'test@test.com',
+    password: 'test'
+  },  function(err, user) {
+      console.log('finished populating user test');
+      console.log('UserId ' + user._id);
+      Group.find({}).remove(function() {
+         Group.create({
+           _creator: user._id,
+           name: "Group de test 1"
+           }, {
+           _creator: user._id,
+           name: "Group de test 2"
+          }
+          )});
+    }
+  )});
+
