8226ed4d9bc2ead4b79f4f566966e7f5af755a14
Alain Beauvois <alain@itipic.com>
2015-04-18 00:31:49 +0200

    Test group controller

--- client/app/group/group.controller.js
+++ client/app/group/group.controller.js
@@ -1,12 +1,11 @@
 'use strict';
 
 angular.module('myappApp')
-  .controller('GroupCtrl', function ($scope, socket, Auth, groups) {
+  .controller('GroupCtrl', function ($scope, groups, socket, Auth) {
     $scope.message = 'Hello';
     $scope.groups = groups;
     var user = Auth.getCurrentUser();
     var tag = 'group_'+user._id;
-    console.log('syncUpdates ' + tag);
     socket.syncUpdates(tag, $scope.groups);
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates(tag);
--- client/app/group/group.controller.spec.js
+++ client/app/group/group.controller.spec.js
@@ -4,11 +4,24 @@ describe('Controller: GroupCtrl', function () {
 
   // load the controller's module
   beforeEach(module('myappApp'));
+  beforeEach(module('socketMock'));
+  beforeEach(module('ngResource'));
 
-  var GroupCtrl, scope;
+  angular.module('myappApp')
+  .provider('groups', function () {
+    this.$get = function() {
+      return [{_id: '111', name: 'Group test 1'}, {_id: '222', name: 'Group test 2'}];
+    };
+  });
+
+
+  var GroupCtrl, scope, Auth;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function (_Auth_,$controller, $rootScope) {
+    Auth = _Auth_;
+    Auth.getCurrentUser = function() { return {_id: 333}; };
+
     scope = $rootScope.$new();
     GroupCtrl = $controller('GroupCtrl', {
       $scope: scope
@@ -16,6 +29,7 @@ describe('Controller: GroupCtrl', function () {
   }));
 
   it('should ...', function () {
-    expect(1).toEqual(1);
+    expect(scope.groups.length).toBe(2);
+    expect(scope.groups[0]._id).toBe('111');
   });
 });
